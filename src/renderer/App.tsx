4c412aaba6c739eb7d11faca539067a30f3dc8b4
setvect <setvect@naver.com>
2023-11-08 22:52:47 +0900

    화면 레이아웃 작업중

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
-import { Col, Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
 
 function Hello() {
   // 추가: 전역 스타일을 위한 CSS-in-JS
@@ -20,56 +21,128 @@ function Hello() {
   };
 
   return (
-    <Container fluid style={fullHeightMinusNavbar}>
-      {/* 상단 바 */}
-      <Row>
-        <Col style={{ paddingRight: 0, paddingLeft: 0 }}>
-          <Navbar ref={navbarRef} bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="#home">복슬가계부</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Col>
-      </Row>
-
-      {/* 본문 영역: 왼쪽 메뉴와 내용 영역 */}
-      <Row style={fullHeightMinusNavbar}>
-        {/* 왼쪽 메뉴 */}
-        <Col xs={2} id="sidebar-wrapper" style={{ ...fullHeightMinusNavbar, padding: 0 }}>
-          <Nav className="flex-column" style={{ ...fullHeightMinusNavbar, background: '#eee' }}>
-            {/* 메뉴 내용 */}
-            <Nav.Item>
-              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#orders">Orders</Nav.Link>
-            </Nav.Item>
-            {/* 추가 메뉴 아이템 */}
-          </Nav>
-        </Col>
-
-        {/* 메인 컨텐츠 영역 */}
-        <Col xs={10} id="page-content-wrapper" style={{ ...fullHeightMinusNavbar, padding: 0 }}>
-          <Container fluid style={{ height: '100%' }}>
-            <h2>Main Content Area</h2>
-            <p>This is the main content area. Replace this text with your own content.</p>
-            {/* 추가 컨텐츠 */}
-          </Container>
-        </Col>
-      </Row>
-    </Container>
+    <div className="container-scroller">
+      <nav className="sidebar sidebar-offcanvas" id="sidebar">
+        <div className="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+          <a className="sidebar-brand brand-logo" href="index.html">
+            <img src="assets/images/logo.svg" alt="logo" />
+          </a>
+          <a className="sidebar-brand brand-logo-mini" href="index.html">
+            <img src="assets/images/logo-mini.svg" alt="logo" />
+          </a>
+        </div>
+        <ul className="nav">
+          <li className="nav-item nav-category"></li>
+          <li className="nav-item menu-items">
+            <a className="nav-link" href="index.html">
+              <span className="menu-icon">
+                <i className="mdi mdi-speedometer"></i>
+              </span>
+              <span className="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li className="nav-item menu-items">
+            <a className="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <span className="menu-icon">
+                <i className="mdi mdi-laptop"></i>
+              </span>
+              <span className="menu-title">Basic UI Elements</span>
+              <i className="menu-arrow"></i>
+            </a>
+            <div className="collapse" id="ui-basic">
+              <ul className="nav flex-column sub-menu">
+                <li className="nav-item">
+                  <a className="nav-link" href="pages/ui-features/buttons.html">
+                    Buttons
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="pages/ui-features/dropdowns.html">
+                    Dropdowns
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="pages/ui-features/typography.html">
+                    Typography
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div className="container-fluid page-body-wrapper">
+        <nav className="navbar p-0 fixed-top d-flex flex-row">
+          <div className="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <a className="navbar-brand brand-logo-mini" href="index.html">
+              <img src="assets/images/logo-mini.svg" alt="logo" />
+            </a>
+          </div>
+          <div className="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
+            <button className="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+              <span className="mdi mdi-menu"></span>
+            </button>
+            <ul className="navbar-nav w-100"></ul>
+            <ul className="navbar-nav navbar-nav-right">
+              <li className="nav-item dropdown">
+                <a className="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
+                  <div className="navbar-profile">
+                    <img className="img-xs rounded-circle" src="assets/images/faces/face15.jpg" alt="" />
+                    <p className="mb-0 d-none d-sm-block navbar-profile-name">복슬이</p>
+                    <i className="mdi mdi-menu-down d-none d-sm-block"></i>
+                  </div>
+                </a>
+                <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="profileDropdown">
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item preview-item">
+                    <div className="preview-thumbnail">
+                      <div className="preview-icon bg-dark rounded-circle">
+                        <i className="mdi mdi-settings text-success"></i>
+                      </div>
+                    </div>
+                    <div className="preview-item-content">
+                      <p className="preview-subject mb-1">Settings</p>
+                    </div>
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item preview-item">
+                    <div className="preview-thumbnail">
+                      <div className="preview-icon bg-dark rounded-circle">
+                        <i className="mdi mdi-logout text-danger"></i>
+                      </div>
+                    </div>
+                    <div className="preview-item-content">
+                      <p className="preview-subject mb-1">Log out</p>
+                    </div>
+                  </a>
+                </div>
+              </li>
+            </ul>
+            <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+              <span className="mdi mdi-format-line-spacing"></span>
+            </button>
+          </div>
+        </nav>
+        <div className="main-panel">
+          <div className="content-wrapper">
+            <div className="page-header">
+              <h3 className="page-title">달력 입력</h3>
+            </div>
+            <div className="row"></div>
+            <div className="row">
+              <div className="col-12">
+                <div className="card">
+                  <div className="card-body">내용</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="d-sm-flex justify-content-center justify-content-sm-between"></div>
+          </footer>
+        </div>
+      </div>
+    </div>
   );
 }
 
