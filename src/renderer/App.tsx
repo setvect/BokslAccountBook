ab9c46e5a97ef70161d8a2fe953412cb2783b865
setvect <setvect@naver.com>
2023-11-09 23:12:39 +0900

    화면 레이아웃 작업중

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,22 +7,9 @@ import './css/style.css';
 function Hello() {
   // 추가: 전역 스타일을 위한 CSS-in-JS
 
-  const navbarRef = useRef<HTMLElement>(null); // 타입을 HTMLElement로 지정
-  const [navbarHeight, setNavbarHeight] = useState(0); // 상단바의 높이 상태
-  useEffect(() => {
-    // navbarRef.current가 존재하는지 확인하고, 존재한다면 높이를 설정합니다.
-    if (navbarRef.current) {
-      setNavbarHeight(navbarRef.current.clientHeight);
-    }
-  }, []); // 빈 의존성 배열을 사용하여 컴포넌트 마운트 시에만 실행
-
-  const fullHeightMinusNavbar = {
-    minHeight: `calc(100vh - ${navbarHeight}px)`,
-  };
-
   return (
     <div className="container-scroller">
-      <nav className="sidebar sidebar-offcanvas" id="sidebar">
+      <Nav className="sidebar sidebar-offcanvas" id="sidebar">
         <div className="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
           <a className="sidebar-brand brand-logo" href="index.html">
             <img src="assets/images/logo.svg" alt="logo" />
@@ -32,11 +19,11 @@ function Hello() {
           </a>
         </div>
         <ul className="nav">
-          <li className="nav-item nav-category"></li>
+          <li className="nav-item nav-category" />
           <li className="nav-item menu-items">
             <a className="nav-link" href="index.html">
               <span className="menu-icon">
-                <i className="mdi mdi-speedometer"></i>
+                <i className="mdi mdi-speedometer" />
               </span>
               <span className="menu-title">Dashboard</span>
             </a>
@@ -44,10 +31,10 @@ function Hello() {
           <li className="nav-item menu-items">
             <a className="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <span className="menu-icon">
-                <i className="mdi mdi-laptop"></i>
+                <i className="mdi mdi-laptop" />
               </span>
               <span className="menu-title">Basic UI Elements</span>
-              <i className="menu-arrow"></i>
+              <i className="menu-arrow" />
             </a>
             <div className="collapse" id="ui-basic">
               <ul className="nav flex-column sub-menu">
@@ -70,7 +57,7 @@ function Hello() {
             </div>
           </li>
         </ul>
-      </nav>
+      </Nav>
       <div className="container-fluid page-body-wrapper">
         <nav className="navbar p-0 fixed-top d-flex flex-row">
           <div className="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
@@ -80,35 +67,35 @@ function Hello() {
           </div>
           <div className="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
             <button className="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-              <span className="mdi mdi-menu"></span>
+              <span className="mdi mdi-menu" />
             </button>
-            <ul className="navbar-nav w-100"></ul>
+            <ul className="navbar-nav w-100" />
             <ul className="navbar-nav navbar-nav-right">
               <li className="nav-item dropdown">
                 <a className="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
                   <div className="navbar-profile">
                     <img className="img-xs rounded-circle" src="assets/images/faces/face15.jpg" alt="" />
                     <p className="mb-0 d-none d-sm-block navbar-profile-name">복슬이</p>
-                    <i className="mdi mdi-menu-down d-none d-sm-block"></i>
+                    <i className="mdi mdi-menu-down d-none d-sm-block" />
                   </div>
                 </a>
                 <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="profileDropdown">
-                  <div className="dropdown-divider"></div>
+                  <div className="dropdown-divider" />
                   <a className="dropdown-item preview-item">
                     <div className="preview-thumbnail">
                       <div className="preview-icon bg-dark rounded-circle">
-                        <i className="mdi mdi-settings text-success"></i>
+                        <i className="mdi mdi-settings text-success" />
                       </div>
                     </div>
                     <div className="preview-item-content">
                       <p className="preview-subject mb-1">Settings</p>
                     </div>
                   </a>
-                  <div className="dropdown-divider"></div>
+                  <div className="dropdown-divider" />
                   <a className="dropdown-item preview-item">
                     <div className="preview-thumbnail">
                       <div className="preview-icon bg-dark rounded-circle">
-                        <i className="mdi mdi-logout text-danger"></i>
+                        <i className="mdi mdi-logout text-danger" />
                       </div>
                     </div>
                     <div className="preview-item-content">
@@ -119,7 +106,7 @@ function Hello() {
               </li>
             </ul>
             <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-              <span className="mdi mdi-format-line-spacing"></span>
+              <span className="mdi mdi-format-line-spacing" />
             </button>
           </div>
         </nav>
@@ -128,17 +115,17 @@ function Hello() {
             <div className="page-header">
               <h3 className="page-title">달력 입력</h3>
             </div>
-            <div className="row"></div>
+            <div className="row" />
             <div className="row">
               <div className="col-12">
                 <div className="card">
-                  <div className="card-body">내용</div>
+                  <div className="card-body">내용11</div>
                 </div>
               </div>
             </div>
           </div>
           <footer className="footer">
-            <div className="d-sm-flex justify-content-center justify-content-sm-between"></div>
+            <div className="d-sm-flex justify-content-center justify-content-sm-between" />
           </footer>
         </div>
       </div>
