f051f277bba856bb082076d19d29f1c255bf872f
setvect <setvect@naver.com>
2023-11-10 15:24:44 +0900

    기본적인 틀 작업

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/theme-dark.css';
 import './css/style.css';
 import TopBar from './TopBar';
 import Menu from './Menu';
-import LedgerCalendar from './page/LedgerCalendar';
-import LedgerTable from './page/LedgerTable';
-import StockTrading from './page/StockTrading';
-import FinancialSettlement from './page/FinancialSettlement';
-import Statistics from './page/Statistics';
-import CategoryManagement from './page/CategoryManagement';
-import AccountManagement from './page/AccountManagement';
-import PurchasedStocks from './page/PurchasedStocks';
-import AssetSnapshot from './page/AssetSnapshot';
-import CodeManagement from './page/CodeManagement';
+
+const LedgerCalendar = React.lazy(() => import('./page/LedgerCalendar'));
+const LedgerTable = React.lazy(() => import('./page/LedgerTable'));
+const StockTrading = React.lazy(() => import('./page/StockTrading'));
+const FinancialSettlement = React.lazy(() => import('./page/FinancialSettlement'));
+const Statistics = React.lazy(() => import('./page/Statistics'));
+const CategoryManagement = React.lazy(() => import('./page/CategoryManagement'));
+const AccountManagement = React.lazy(() => import('./page/AccountManagement'));
+const PurchasedStocks = React.lazy(() => import('./page/PurchasedStocks'));
+const AssetSnapshot = React.lazy(() => import('./page/AssetSnapshot'));
+const CodeManagement = React.lazy(() => import('./page/CodeManagement'));
 
 function Main() {
   const navbarRef = useRef<HTMLElement>(null);
@@ -38,22 +39,34 @@ function Main() {
         <Router>
           <Menu bodyHeight={bodyHeight} />
           <Col style={{ ...bodyHeight, padding: '20px' }} className="color-theme-content-bg">
-            <Routes>
-              <Route path="/" element={<LedgerCalendar />} />
-              <Route path="/LedgerTable" element={<LedgerTable />} />
-              <Route path="/StockTrading" element={<StockTrading />} />
-              <Route path="/FinancialSettlement" element={<FinancialSettlement />} />
-              <Route path="/Statistics" element={<Statistics />} />
-              <Route path="/CategoryManagement" element={<CategoryManagement />} />
-              <Route path="/AccountManagement" element={<AccountManagement />} />
-              <Route path="/PurchasedStocks" element={<PurchasedStocks />} />
-              <Route path="/AssetSnapshot" element={<AssetSnapshot />} />
-              <Route path="/CodeManagement" element={<CodeManagement />} />
-            </Routes>
+            <Suspense fallback={<Wait />}>
+              <Routes>
+                <Route path="/" element={<LedgerCalendar />} />
+                <Route path="/LedgerTable" element={<LedgerTable />} />
+                <Route path="/StockTrading" element={<StockTrading />} />
+                <Route path="/FinancialSettlement" element={<FinancialSettlement />} />
+                <Route path="/Statistics" element={<Statistics />} />
+                <Route path="/CategoryManagement" element={<CategoryManagement />} />
+                <Route path="/AccountManagement" element={<AccountManagement />} />
+                <Route path="/PurchasedStocks" element={<PurchasedStocks />} />
+                <Route path="/AssetSnapshot" element={<AssetSnapshot />} />
+                <Route path="/CodeManagement" element={<CodeManagement />} />
+              </Routes>
+            </Suspense>
           </Col>
         </Router>
       </Row>
     </Container>
+  );
+}
+
+function Wait() {
+  return (
+    <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
--- src/renderer/Menu.tsx
+++ src/renderer/Menu.tsx
@@ -12,66 +12,43 @@ import {
   FaUniversity,
 } from 'react-icons/fa';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 interface MenuProps {
   bodyHeight: { minHeight: string };
 }
 
+const menuItems = [
+  { path: '/', label: '가계부 쓰기(달력)', icon: <FaCalendarAlt className="me-2" /> },
+  { path: '/LedgerTable', label: '가계부 쓰기(표)', icon: <FaTable className="me-2" /> },
+  { path: '/StockTrading', label: '주식 매매', icon: <FaChartLine className="me-2" /> },
+  { path: '/FinancialSettlement', label: '결산', icon: <FaBalanceScale className="me-2" /> },
+  { path: '/Statistics', label: '통계', icon: <FaChartPie className="me-2" /> },
+  { path: '/CategoryManagement', label: '분류 관리', icon: <FaTags className="me-2" /> },
+  { path: '/AccountManagement', label: '계좌 관리', icon: <FaUniversity className="me-2" /> },
+  { path: '/PurchasedStocks', label: '매수 종목', icon: <FaRegListAlt className="me-2" /> },
+  { path: '/AssetSnapshot', label: '자산 스냅샷', icon: <FaCamera className="me-2" /> },
+  { path: '/CodeManagement', label: '코드 관리', icon: <FaCode className="me-2" /> },
+];
+
 function Menu({ bodyHeight }: MenuProps) {
+  const location = useLocation();
+
+  const getButtonClass = (path: string) => {
+    return location.pathname === path ? 'custom-btn-navy' : 'custom-btn';
+  };
+
   return (
     <Col className="color-theme-left sidebar-style">
       <Nav className="flex-column" style={{ ...bodyHeight, padding: '20px 20px' }}>
-        <Link to="/">
-          <Button className="text-left mb-2 menu-button custom-btn-navy">
-            <FaCalendarAlt className="me-2" /> 가계부 쓰기(달력)
-          </Button>
-        </Link>
-        <Link to="/LedgerTable">
-          <Button className="text-left mb-2 menu-button custom-btn">
-            <FaTable className="me-2" /> 가계부 쓰기(표)
-          </Button>
-        </Link>
-        <Link to="/StockTrading">
-          <Button className="text-left mb-2 menu-button custom-btn">
-            <FaChartLine className="me-2" /> 주식 매매
-          </Button>
-        </Link>
-        <Link to="/FinancialSettlement">
-          <Button className="text-left mb-2 menu-button custom-btn">
-            <FaBalanceScale className="me-2" /> 결산
-          </Button>
-        </Link>
-        <Link to="/Statistics">
-          <Button className="text-left mb-2 menu-button custom-btn">
-            <FaChartPie className="me-2" /> 통계
-          </Button>
-        </Link>
-        <Link to="/CategoryManagement">
-          <Button className=" text-left mb-2 menu-button custom-btn">
-            <FaTags className=" me-2" /> 분류 관리
-          </Button>
-        </Link>
-        <Link to="/AccountManagement">
-          <Button className="text-left mb-2 menu-button custom-btn">
-            <FaUniversity className="me-2" /> 계좌 관리
-          </Button>
-        </Link>
-        <Link to="/PurchasedStocks">
-          <Button className=" text-left mb-2 menu-button custom-btn">
-            <FaRegListAlt className=" me-2" /> 매수 종목
-          </Button>
-        </Link>
-        <Link to="/AssetSnapshot">
-          <Button className="text-left mb-2 menu-button custom-btn">
-            <FaCamera className="me-2" /> 자산 스냅샷
-          </Button>
-        </Link>
-        <Link to="/CodeManagement">
-          <Button className=" text-left mb-2 menu-button custom-btn">
-            <FaCode className=" me-2" /> 코드 관리
-          </Button>
-        </Link>
+        {menuItems.map((item) => (
+          <Link to={item.path} key={item.path}>
+            <Button className={`text-left mb-2 menu-button ${getButtonClass(item.path)}`}>
+              {item.icon}
+              {item.label}
+            </Button>
+          </Link>
+        ))}
       </Nav>
     </Col>
   );
