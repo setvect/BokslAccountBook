d3fec7348c89d49017a24c6f9e1067c326253a01
setvect <setvect@naver.com>
2023-11-19 10:14:49 +0900

    거래 내역 모달창 - select

--- src/renderer/components/common/TransactionAddModal.tsx
+++ src/renderer/components/common/TransactionAddModal.tsx
@@ -1,6 +1,8 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
-import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
+import { Button, Col, Form, InputGroup, Modal, Row } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
+import Select, { GroupBase, StylesConfig } from 'react-select';
+import { NumericFormat } from 'react-number-format';
 import { AccountType, Kind, TransactionModalForm } from './BokslTypes';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -8,6 +10,50 @@ export interface TransactionAddModalHandle {
   openModal: (type: AccountType, item: TransactionModalForm, saveCallback: () => void) => void;
   hideModal: () => void;
 }
+
+interface OptionType {
+  value: number;
+  label: string;
+}
+
+export interface ColourOption {
+  readonly value: string;
+  readonly label: string;
+  readonly color: string;
+  readonly isFixed?: boolean;
+  readonly isDisabled?: boolean;
+}
+
+const darkThemeStyles: StylesConfig<OptionType, false, GroupBase<OptionType>> = {
+  control: (styles) => ({
+    ...styles,
+    backgroundColor: '#212529',
+    color: '#dee2e6',
+    borderColor: '#495057',
+  }),
+  singleValue: (styles) => ({
+    ...styles,
+    color: '#dee2e6',
+  }),
+  menu: (styles) => ({
+    ...styles,
+    backgroundColor: '#212529',
+    color: 'white',
+  }),
+  option: (styles, { isFocused, isSelected }) => {
+    let backgroundColor = 'black';
+    if (isFocused) {
+      backgroundColor = '#33394a';
+    } else if (isSelected) {
+      backgroundColor = '#5d5e5f';
+    }
+    return {
+      ...styles,
+      backgroundColor,
+      color: '#8e9092',
+    };
+  },
+};
 
 const TransactionAddModal = forwardRef<TransactionAddModalHandle, {}>((props, ref) => {
   const [showModal, setShowModal] = useState(false);
@@ -38,9 +84,24 @@ const TransactionAddModal = forwardRef<TransactionAddModalHandle, {}>((props, re
     hideModal: () => setShowModal(false),
   }));
 
+  const options = [
+    { value: 1, label: '계좌 1' },
+    { value: 2, label: '계좌 2' },
+    { value: 3, label: '계좌 3' },
+  ];
+
   useEffect(() => {
     console.log(form);
   }, [form]);
+
+  function changeTransactionDate(diff: number) {
+    const d = form.transactionDate.getDate() + diff;
+    form.transactionDate.setDate(d);
+    setForm((prevForm) => ({
+      ...prevForm,
+      transactionDate: form.transactionDate,
+    }));
+  }
 
   return (
     <Modal show={showModal} onHide={() => setShowModal(false)} size="lg" dialogClassName="modal-xl" centered data-bs-theme="dark">
@@ -48,23 +109,130 @@ const TransactionAddModal = forwardRef<TransactionAddModalHandle, {}>((props, re
         <Modal.Title>지출 내역 등록 {type}</Modal.Title>
       </Modal.Header>
       <Modal.Body className="bg-dark text-white-50">
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+                <Form.Label column sm={2}>
+                  날짜
+                </Form.Label>
+                <Col sm={10}>
+                  <Row>
+                    <Col sm={8}>
+                      <div className="form-group">
+                        <DatePicker
+                          dateFormat="yyyy-MM-dd"
+                          selected={form.transactionDate}
+                          onChange={(date) => {
+                            setForm((prevForm) => ({
+                              ...prevForm,
+                              transactionDate: date || new Date(),
+                            }));
+                          }}
+                          className="form-control"
+                        />
+                      </div>
+                    </Col>
+                    <Col>
+                      <Button variant="outline-success" onClick={() => changeTransactionDate(-1)}>
+                        전날
+                      </Button>
+                      <Button variant="outline-success" style={{ marginLeft: '5px' }} onClick={() => changeTransactionDate(1)}>
+                        다음날
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                <Form.Label column sm={2}>
+                  항목
+                </Form.Label>
+                <Col sm={10}>
+                  <InputGroup className="mb-3">
+                    <Form.Control readOnly />
+                    <Button variant="outline-secondary" id="button-addon2">
+                      선택
+                    </Button>
+                  </InputGroup>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                <Form.Label column sm={2}>
+                  메모
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control onChange={(e) => setForm((prevForm) => ({ ...prevForm, note: e.target.value }))} value={form.note} />
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                <Form.Label column sm={2}>
+                  금액
+                </Form.Label>
+                <Col sm={10}>
+                  <NumericFormat
+                    thousandSeparator
+                    onValueChange={(values) => setForm((prevForm) => ({ ...prevForm, money: values.floatValue || 0 }))}
+                    value={form.money}
+                    maxLength={12}
+                    className="form-control"
+                    style={{ textAlign: 'right' }}
+                  />
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                <Form.Label column sm={2}>
+                  지출계좌
+                </Form.Label>
+                <Col sm={10}>
+                  <Select<OptionType, false, GroupBase<OptionType>>
+                    value={options.find((option) => option.value === form.payAccount)}
+                    onChange={(option) =>
+                      setForm((prevForm) => ({
+                        ...prevForm,
+                        payAccount: option ? option.value : 0,
+                      }))
+                    }
+                    options={options}
+                    placeholder="계좌 선택"
+                    className="react-select-container"
+                    styles={darkThemeStyles}
+                    isClearable
+                  />
+                </Col>
+              </Form.Group>
+              <fieldset>
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label as="legend" column sm={2}>
+                    Radios
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Check type="radio" label="first radio" name="formHorizontalRadios" id="formHorizontalRadios1" />
+                    <Form.Check type="radio" label="second radio" name="formHorizontalRadios" id="formHorizontalRadios2" />
+                    <Form.Check type="radio" label="third radio" name="formHorizontalRadios" id="formHorizontalRadios3" />
+                  </Col>
+                </Form.Group>
+              </fieldset>
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+                <Col sm={{ span: 10, offset: 2 }}>
+                  <Form.Check label="Remember me" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} className="mb-3">
+                <Col sm={{ span: 10, offset: 2 }}>
+                  <Button type="submit">Sign in</Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
         <Form>
           <Row>
             <Form.Group as={Col}>
               <Form.Label>날짜:</Form.Label>
-              <div className="form-group">
-                <DatePicker
-                  dateFormat="yyyy-MM-dd"
-                  selected={form.transactionDate}
-                  onChange={(date) => {
-                    setForm((prevForm) => ({
-                      ...prevForm,
-                      transactionDate: date || new Date(),
-                    }));
-                  }}
-                  className="form-control"
-                />
-              </div>
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>금액:</Form.Label>
