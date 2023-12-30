fd3dc8b7e248bb6034f2cd625d0785c8fcf3c698
setvect <setvect@naver.com>
2023-12-30 23:00:11 +0900

    거래 CRUD - 거래 내역 통계 - GPT4를 이용

--- src/renderer/components/table/TransactionSummary.tsx
+++ src/renderer/components/table/TransactionSummary.tsx
@@ -1,6 +1,6 @@
 import { Table } from 'react-bootstrap';
 import React from 'react';
-import { CurrencyProperties, CurrencySumModel } from '../../common/RendererModel';
+import { CurrencyProperties } from '../../common/RendererModel';
 import { convertToCommaSymbol } from '../util/util';
 import { ResTransactionModel } from '../../../common/ResModel';
 import { Currency, TransactionKind } from '../../../common/CommonType';
@@ -10,72 +10,38 @@ interface TransactionSummaryProps {
 }
 
 function TransactionSummary({ transactionList }: TransactionSummaryProps) {
-  const getCurrencySum = (transactionList: ResTransactionModel[]) => {
-    // 통화별 합계
-    const currencySumList: CurrencySumModel[] = [];
-    Object.values(Currency).forEach((currency) => {
-      const sum = transactionList.reduce((acc, transaction) => {
-        if (transaction.currency === currency) {
-          return acc + transaction.amount;
-        }
-        return acc;
-      }, 0);
-      if (sum > 0) {
-        currencySumList.push({ currency, amount: sum });
-      }
+  const calculateSumByType = (kind: TransactionKind) => {
+    const filteredList = transactionList.filter((transaction) => transaction.kind === kind);
+    return Object.values(Currency)
+      .map((currency) => {
+        const sum = filteredList.reduce((acc, transaction) => {
+          return transaction.currency === currency ? acc + transaction.amount : acc;
+        }, 0);
+        return { currency, amount: sum };
+      })
+      .filter((currencySum) => currencySum.amount !== 0);
+  };
+
+  const spendingSum = calculateSumByType(TransactionKind.SPENDING);
+  const incomeSum = calculateSumByType(TransactionKind.INCOME);
+  const transferSum = calculateSumByType(TransactionKind.TRANSFER);
+
+  const incomeSubtractSpending = Object.values(Currency)
+    .filter((currency) => {
+      const incomeAmount = incomeSum.find((sum) => sum.currency === currency);
+      const spendingAmount = spendingSum.find((sum) => sum.currency === currency);
+      return incomeAmount || spendingAmount;
+    })
+    .map((currency) => {
+      const incomeAmount = incomeSum.find((sum) => sum.currency === currency)?.amount || 0;
+      const spendingAmount = spendingSum.find((sum) => sum.currency === currency)?.amount || 0;
+      return { currency, amount: incomeAmount - spendingAmount };
     });
-    return currencySumList;
-  };
-
-  const getSpendingSum = () => {
-    const spendingList = transactionList.filter((transaction) => transaction.kind === TransactionKind.SPENDING);
-    return getCurrencySum(spendingList);
-  };
-
-  const getIncomeSum = () => {
-    const incomeList = transactionList.filter((transaction) => transaction.kind === TransactionKind.INCOME);
-    return getCurrencySum(incomeList);
-  };
-
-  const getTransferSum = () => {
-    const transferList = transactionList.filter((transaction) => transaction.kind === TransactionKind.TRANSFER);
-    return getCurrencySum(transferList);
-  };
-
-  const getIncomeSubtractSpending = () => {
-    const incomeList = getIncomeSum();
-    const spendingList = getSpendingSum();
-
-    const currencySet = new Set<Currency>();
-
-    incomeList.forEach((income) => {
-      currencySet.add(income.currency);
-    });
-
-    spendingList.forEach((spending) => {
-      currencySet.add(spending.currency);
-    });
-
-    const incomeSubtractSpendingList: CurrencySumModel[] = [];
-
-    currencySet.forEach((currency) => {
-      const income = incomeList.find((income) => income.currency === currency);
-      const incomeAmount = income ? income.amount : 0;
-
-      const spending = spendingList.find((spending) => spending.currency === currency);
-      const spendingAmount = spending ? spending.amount : 0;
-
-      const incomeSubtractSpending = incomeAmount - spendingAmount;
-      incomeSubtractSpendingList.push({ currency, amount: incomeSubtractSpending });
-    });
-
-    return incomeSubtractSpendingList;
-  };
 
   return (
     <Table striped bordered hover variant="dark" className="table-th-center table-font-size">
       <tbody>
-        {getSpendingSum().map((currencySum) => (
+        {spendingSum.map((currencySum) => (
           <tr key={currencySum.currency}>
             <td>
               <span className="account-spending">지출({CurrencyProperties[currencySum.currency].name})</span>
@@ -83,7 +49,7 @@ function TransactionSummary({ transactionList }: TransactionSummaryProps) {
             <td className="right">{convertToCommaSymbol(currencySum.amount, currencySum.currency)}</td>
           </tr>
         ))}
-        {getIncomeSum().map((currencySum) => (
+        {incomeSum.map((currencySum) => (
           <tr key={currencySum.currency}>
             <td>
               <span className="account-income">수입({CurrencyProperties[currencySum.currency].name})</span>
@@ -91,7 +57,7 @@ function TransactionSummary({ transactionList }: TransactionSummaryProps) {
             <td className="right">{convertToCommaSymbol(currencySum.amount, currencySum.currency)}</td>
           </tr>
         ))}
-        {getIncomeSubtractSpending().map((currencySum) => (
+        {incomeSubtractSpending.map((currencySum) => (
           <tr key={currencySum.currency}>
             <td>
               <span className="account-income">수입({CurrencyProperties[currencySum.currency].name})</span> -
@@ -100,7 +66,7 @@ function TransactionSummary({ transactionList }: TransactionSummaryProps) {
             <td className="right">{convertToCommaSymbol(currencySum.amount, currencySum.currency)}</td>
           </tr>
         ))}
-        {getTransferSum().map((currencySum) => (
+        {transferSum.map((currencySum) => (
           <tr key={currencySum.currency}>
             <td>
               <span className="account-transfer"> 이체({CurrencyProperties[currencySum.currency].name})</span>
